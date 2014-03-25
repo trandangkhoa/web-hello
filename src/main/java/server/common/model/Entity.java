eabece61418b8f2b0ce35cfd2ebd916920b675b7
trandangkhoa <tetsusoiwashiro@gmail.com>
2014-03-25 19:53:42 +0700

    Initial commit

---
+++ src/main/java/server/common/model/Entity.java
@@ -0,0 +1,14 @@
+package server.common.model;
+
+public class Entity {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/server/module/service/EntityServlet.java
@@ -0,0 +1,26 @@
+package server.module.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EntityServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("text/html");
+		resp.getWriter().write("Hello world");
+	}
+
+}
